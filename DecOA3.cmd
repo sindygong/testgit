2d1d0c0921ecf6b8ab95ce412cb12fd4fb3c57e7
gonghf <20103229@qq.com>
2018-01-08 12:31:01 +0800

    Signed-off-by: gonghf <20103229@qq.com>

--- DecOA3.cmd
+++ DecOA3.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 rem add changes for verification test-1
-rem add changes for verification test-3
+rem add changes for verification test-4
 
 if %PROCESSOR_ARCHITECTURE% == AMD64 SET OSBIT=64
 if %PROCESSOR_ARCHITECTURE% == x86 SET OSBIT=32
